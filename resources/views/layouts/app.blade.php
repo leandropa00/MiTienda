2dc2746cab7cacbc6bc31495ee5df659f7af7c5a
leandropa00 <leandropa00@gmail.com>
2021-12-10 12:51:24 -0500

    Ajuste sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,12 +33,14 @@
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav mr-auto">
-                            <li class="nav-item">
-                                <a class="nav-link @if(Route::is('productos*')) active @endif" href="{{ route('productos.index') }}">Productos</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link @if(Route::is('facturas*')) active @endif" href="{{ route('facturas.index') }}">Facturas</a>
-                            </li>
+                            @auth
+                                <li class="nav-item">
+                                    <a class="nav-link @if(Route::is('productos*')) active @endif" href="{{ route('productos.index') }}">Productos</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link @if(Route::is('facturas*')) active @endif" href="{{ route('facturas.index') }}">Facturas</a>
+                                </li>
+                            @endauth
                         </ul>
                         <ul class="navbar-nav ml-auto">
                             @guest
